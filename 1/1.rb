1d7c2cc5d2a7b63af133eb3c03f5b6586733992d
heydanhey <danhey@gmail.com>
2018-02-23 09:23:39 -0600

    refactored day one

--- 1/1.rb
+++ 1/1.rb
@@ -1,12 +1,9 @@
 # DayOne Checksum
 class DayOne
   def self.sum_of_equal_neighbors(input)
-    equal_neighbors = []
-    arr = input.to_s.chars
-    arr.each_with_index do |char, index|
-      neighbor_index = index + 1 == arr.length ? 0 : index + 1
-      equal_neighbors << char.to_i if char == arr[neighbor_index]
-    end
-    equal_neighbors.inject(0, :+)
+    input
+      .digits
+      .select.with_index{ |d, i| d == input.digits[i - 1] }
+      .inject(0, :+)
   end
 end
